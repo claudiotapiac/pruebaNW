201a3802548cafc4a0308f9a64dbfe3e71097898
claudiotapiac <claudio.tapia.c@mail.pucv.cl>
2023-04-09 00:16:36 -0400

    Se agregó modelo RandomForest

--- app.py
+++ app.py
@@ -1,39 +1,65 @@
-
 from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
 import torch
-import torch.nn as nn
 from model_atraso import AutoencoderClassifier
 from flask_cors import CORS
-from datetime import datetime, timezone, timedelta
 import pandas as pd
-import pytz
-import os
+import pickle
+from zipfile import ZipFile
+
+
+model_rf_extract = ZipFile('./models/model_rf.zip')
+model_rf_extract.extract('model_rf.pickle', './models/')
 
 app = Flask(__name__)
 CORS(app)
 
-model = AutoencoderClassifier(8)
-model.load_state_dict(torch.load('./weight/model.pth')) 
-model.eval()
+try:
+  rf_model = pickle.load(open('./models/model_rf.pickle', 'rb'))
+except:
+  print("No existe archivo para el modelo de RandomForest")
 
-def str_2_tensor(input_model):
+torch_model = AutoencoderClassifier(8)
+torch_model.load_state_dict(torch.load('./models/model_pytorch.pth')) 
+torch_model.eval()
+
+
+def str_2_model(input_model, type = "sklearn"):
   for i, value in enumerate(input_model):
     input_model[i] = float(value)
-  input_model = torch.tensor(input_model, dtype=torch.float32)
+  if type == "pytorch":
+    input_model = torch.tensor(input_model, dtype=torch.float32)
+  else:
+    input_model = pd.DataFrame(input_model).T
   return input_model
 
-@app.route('/',methods = ['POST', 'GET'])
-def get():
+
+@app.route('/torch/',methods = ['POST', 'GET'])
+def pytorch():
   input_model = request.form.to_dict(flat=False)
   input_model = input_model['values']
-  input_model = str_2_tensor(input_model)
+  input_model = str_2_model(input_model, "pytorch")
 
   with torch.no_grad():
-    output = model(input_model)
-    print(output)
+    output = torch_model(input_model)
+
   result = {'value': output.item()}
   return jsonify(result)
+
+
+@app.route('/rf/',methods = ['POST', 'GET'])
+def rf():
+  try:
+    input_model = request.form.to_dict(flat=False)
+    input_model = input_model['values']
+    input_model = str_2_model(input_model, "sklearn")
+    
+    output = rf_model.predict(input_model)
+
+    result = {'value': output.item()}
+  except:
+    result = {}
+  return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
